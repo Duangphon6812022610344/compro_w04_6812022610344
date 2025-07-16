65a30fcf32840a84b839ff5aa71c3504d12952b7
Duangphon6812022610344 <s6812022610344@email.kmutnb.ac.th>
2025-07-16 14:38:03 +0700

    Add files via upload

---
+++ coding_w04-01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+    int main() { // main(Identifier),int(Keyword)
+        printf("Hello\n"); // printf(Identifier)
+        printf("C Tokens!\n"); // printf(Identifier)
+        return 0; // return(Keyword)
+    }
